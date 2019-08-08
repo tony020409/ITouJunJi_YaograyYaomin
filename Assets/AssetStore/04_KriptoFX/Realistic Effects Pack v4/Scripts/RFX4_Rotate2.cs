45536202284771651aa78006d02c434516f033a7
Tony <tony020409@gmail.com>
2019-08-08 14:01:43 +0800

    111

---
+++ Assets/AssetStore/04_KriptoFX/Realistic Effects Pack v4/Scripts/RFX4_Rotate2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RFX4_Rotate2 : MonoBehaviour {
+
+
+    public Vector3 RotateVector = Vector3.forward;
+    private Transform t;
+
+    // Use this for initialization
+    void Start () {
+        t = transform;
+    }
+
+
+     void Update() {
+        t.Rotate(RotateVector * Time.deltaTime);
+    }
+
+
+}
