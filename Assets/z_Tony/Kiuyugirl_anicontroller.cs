8668560cba433ac74d97726bfd6e7ef69455462e
Tony <tony020409@gmail.com>
2019-08-16 18:11:56 +0800

    0816 9

---
+++ Assets/z_Tony/Kiuyugirl_anicontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kiuyugirl_anicontroller : MonoBehaviour {
+
+    public Animator ani;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ani.Play("idle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ani.Play("action");
+        }
+
+    }
+
+
+    
+}
