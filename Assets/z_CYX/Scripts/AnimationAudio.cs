2250497e12e4edc3611c1b42417143c50cd23b4f
Tony <tony020409@gmail.com>
2019-08-16 16:02:43 +0800

    8163

---
+++ Assets/z_CYX/Scripts/AnimationAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationAudio : MonoBehaviour {
+
+    public void PlayAudio (string _AudioName) {
+        if (_AudioName != null && _AudioName != "") {
+            AudioManager._ins.PlayAudioClip("AnimationAudio", transform.position, _AudioName);
+        }
+    }
+}
---
+++ Assets/z_Tony/Uan_anicontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uan_anicontroller : MonoBehaviour {
+
+    public Animator ani;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ani.Play("idle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ani.Play("attack");
+        }
+
+    }
+
+
+    
+}
