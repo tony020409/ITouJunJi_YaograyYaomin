3469e4edadd53108e8588d1eed730dadfd21ca7f
Tony <tony020409@gmail.com>
2019-09-24 11:51:52 +0800

    190924_0

--- Assets/z_Tony/All_anicontroller.cs
+++ Assets/z_Tony/All_anicontroller.cs
@@ -6,21 +6,28 @@ using UnityEngine;
 public class All_anicontroller : MonoBehaviour {
 
     public Animator ani;
-
+    public AimIK aimik;
     public string ani1;
     public string ani2;
     public string ani3;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        if (gameObject.name == "FuFace_9F")
+        {
+            aimik.enabled = false;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Q))
         {
             ani.Play(ani1);
+            if(gameObject.name == "FuFace_9F")
+            {
+                aimik.enabled = false;
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.W))
@@ -28,7 +35,7 @@ public class All_anicontroller : MonoBehaviour {
             ani.Play(ani2);
             if (gameObject.name == "FuFace_9F")
             {
-                GetComponent<AimIK>().enabled = true;
+                aimik.enabled = true;
             }
         }
         if (Input.GetKeyDown(KeyCode.E))
---
+++ Assets/z_Tony/FU09_callback.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+
+public class FU09_callback : MonoBehaviour {
+
+    public AimIK aimik;
+
+	// Use this for initialization
+	void Start () {
+        aimik.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void open_aimik()
+    {
+        aimik.enabled = true;
+    }
+
+}
